c3757780070bdb7e97778027ebf2bd89a3a63604
Gogota <gilhermeAS1@gmail.com>
2023-02-28 21:42:59 -0300

    exercicio 1

--- basico/01.js
+++ basico/01.js
@@ -1,4 +1,21 @@
 /**
  * 1. Faça um algoritmo que receba o nome e idade do usuário depois mostre ele na tela
- */
-console.log("Ola mundo")
+ * 
+ * 1. Receber nome X
+ * 2. Receber idade X
+ * 3. Mostrar nome na tela X
+ * 4. Mostrar idade na tela X
+*/
+
+// Importando o pacote
+var prompt = require('prompt-sync')()
+// // Usando o pacote para receber um valor do usuário
+// var name = prompt('Qual seu nome ?')
+// // Usamos o + para juntar uma string(texto) com uma variável
+// console.log('Oi ' + name)
+
+var name = prompt('qual o seu nome?')
+var idade = prompt('quel é sua idade?')
+console.log('seu nome é: ' + name)
+console.log('sua idade é: ' + idade + ' anos')
+
